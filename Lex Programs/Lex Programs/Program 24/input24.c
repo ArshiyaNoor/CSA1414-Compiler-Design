d1cdb21c44ca653b2078d3c51955f1c381d261f3
Arshiya Noor <arshiyanoormohamed@gmail.com>
2026-08-12 01:51:14 +0530

    Add files via upload

---
+++ Lex Programs/Lex Programs/Program 24/input24.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c; /* variable declaration */
+printf("Enter two numbers");
+scanf("%d %d",&a,&b);
+c=a+b; // adding two numbers
+printf("Sum is %d",c);
+return 0;
+}
